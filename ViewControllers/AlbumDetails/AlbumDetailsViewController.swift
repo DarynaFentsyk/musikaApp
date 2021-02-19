204dcf9277c50701331b77053bd2fcda8fba4074
Daryna Fentsyk <34345954+DarynaFentsyk@users.noreply.github.com>
2021-02-19 13:22:18 +0100

    Update AlbumDetailsViewController.swift

--- ViewControllers/AlbumDetails/AlbumDetailsViewController.swift
+++ ViewControllers/AlbumDetails/AlbumDetailsViewController.swift
@@ -25,7 +25,6 @@ final class AlbumDetailsViewController: BaseViewController {
         
         self.logicController.loadAlbumInfo()
         self.setupUI()
-    
     }
     
     private func setupUI() {
